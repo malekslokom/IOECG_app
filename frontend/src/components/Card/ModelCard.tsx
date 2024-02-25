4bcfa566b9ec8657987b8f5fded4e56e1aab9e98
malekslokom <slokom.malek@gmail.com>
2024-02-26 00:07:28 +0100

    modal with bootstrap

--- frontend/src/components/Card/ModelCard.tsx
+++ frontend/src/components/Card/ModelCard.tsx
@@ -1,6 +1,4 @@
-// components/Card/ModelCard.tsx
 import React from 'react';
-import "./ModelCard.css";
 
 interface ModelCardProps {
   model: Model;
@@ -9,18 +7,24 @@ interface ModelCardProps {
 
 const ModelCard: React.FC<ModelCardProps> = ({ model, onClick }) => {
   return (
-    <div className="card-container" onClick={onClick}> 
-      <div className="card" style={{ width: '18rem' }}>
-        <div className="card-body">
-          <h5 className="card-title">{model.name}</h5>
-          <h6 className="card-subtitle mb-2 text-body-secondary">{model.author}</h6>
-          <p className="card-text">{model.description}</p>
-          <hr/>
-          <span className="tag">{model.taskNature}</span>
-        </div>
-      </div>
-    </div>
-    
+      // <div className="card card-container " onClick={onClick}> 
+      //   <div className="card-body">
+      //     <h5 className="card-title">{model.name}</h5>
+      //     <hr/>
+      //     <h6 className="card-subtitle mb-2 text-muted">{model.author}</h6>
+      //     <p className="card-text" style={{ height: '70px', overflow: 'hidden' }}>{model.description}</p>
+      //     <span className="badge bg-secondary">{model.taskNature}</span>
+      //   </div>
+      // </div>
+      
+          <div className="card" onClick={onClick}>
+            <div className="card-block">
+              <h4 className="card-title">{model.name}</h4>
+              <h6 className="card-subtitle text-muted">{model.author}</h6>
+              <p className="card-text p-y-1">{model.description}</p>
+              <span className="badge bg-secondary p-2">{model.taskNature}</span>
+            </div>
+          </div>
   );
 }
 
--- frontend/src/components/Modals/ModelModal.tsx
+++ frontend/src/components/Modals/ModelModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Modal, Button, Input, Flex, Tag } from 'antd';
+// import { Modal, Button, Input, Flex, Tag } from 'antd';
 
 import './ModelModal.css';
 
@@ -10,97 +10,99 @@ interface ModelModalProps {
 
 const ModelModal: React.FC<ModelModalProps> = ({ model, onClose }) => {
   return (
-    <Modal
-      title={model.name}
-      visible={true}
-      onCancel={onClose}
-      footer={null}
-      width={600}
-    >
-
+    // <Modal
+    //   title={model.name}
+    //   visible={true}
+    //   onCancel={onClose}
+    //   footer={null}
+    //   width={600}
+    // >
+<div className="modal fade" id="exampleModal" tabIndex={-1} role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog" role="document">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">{model.name}</h5>
+        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
       <div className="modal-body">       
         <div className="modal-section">
           <p className="modal-section-title">Informations sur le modèle</p>
           <div className="section-box">
-            <Flex justify="space-between">
-              <div>
-                <Input addonBefore="Auteur" value={model.author} readOnly />
-                <Input addonBefore="Nom de projet" value={model.projectName} readOnly />
+          <div className="d-flex justify-content-between">
+              <div className="input-group mb-3">
+                <span className="input-group-text">Auteur</span>
+                <input type="text" className="form-control" value="{model.author}" readOnly/>
               </div>
-            </Flex>
-            <Input.TextArea
-              rows={4}
-              value={model.description}
-              autoSize={{ minRows: 4, maxRows: 8 }}
-              readOnly
-            />
+              <div className="input-group mb-3">
+                <span className="input-group-text">Nom de projet</span>
+                <input type="text" className="form-control" value="{model.projectName}" readOnly/>
+              </div>
           </div>
-        </div>
+          <textarea className="form-control" rows={4} readOnly>{model.description}</textarea>
 
-        <div className="modal-section">
-          <p className="modal-section-title">Architecture</p>
-          <div className="section-box">
-            <Flex justify="space-between">
-              <div>
-                <Input addonBefore="Nom" value={model.architectureName} readOnly />
-                <Input addonBefore="Version" value={model.architectureVersion} readOnly />
-              </div>
-            </Flex>
-            <Input.TextArea
-              rows={4}
-              value={model.architectureDescription}
-              autoSize={{ minRows: 4, maxRows: 8 }}
-              readOnly
-            />
           </div>
         </div>
-        <div className="modal-section">
+      </div>
+      <div className="modal-section">
+        <p className="modal-section-title">Architecture</p>
+        <div className="section-box">
+          <div className="d-flex justify-content-between">
+            <div className="input-group mb-3">
+              <span className="input-group-text">Nom</span>
+              <input type="text" className="form-control" value="{model.architectureName}" readOnly/>
+            </div>
+            <div className="input-group mb-3">
+              <span className="input-group-text">Version</span>
+              <input type="text" className="form-control" value="{model.architectureVersion}" readOnly/>
+            </div>
+          </div>
+          <textarea className="form-control" rows={4} readOnly>{model.architectureDescription}</textarea>
+        </div>
+      </div>
+
+      <div className="modal-section">
         <p className="modal-section-title">Informations Additionnelles</p>
         <div className="section-box">
-          <Flex wrap="wrap" gap="small"  justify="space-between" style={{ marginBottom: '20px' }}>
-          
+          <div className="d-flex flex-wrap gap-2 justify-content-between mb-4">
             <div>
               <h4 className="tag-title">Total des Paramètres</h4>
-              <Tag className='tag'>{model.totalParams}</Tag>
-              
+              <span className="badge bg-secondary">{model.totalParams}</span>
             </div>
-            
             <div>
               <h4 className="tag-title">Taille du Modèle</h4>
-              <Tag className='tag'>{model.modelSize}</Tag>
-              
+              <span className="badge bg-secondary">{model.modelSize}</span>
             </div>
             <div>
               <h4 className="tag-title">Taille du Batch</h4>
-                <Tag className='tag'>{model.hyperparameters.batchSize}</Tag>
+              <span className="badge bg-secondary">{model.hyperparameters.batchSize}</span>
             </div>
-            </Flex>
-
-
-
-            <Flex wrap="wrap" gap="small"  justify="space-between" >
-          
+          </div>
+          <div className="d-flex flex-wrap gap-2 justify-content-between">
             <div>
-               <h4 className="tag-title">Taux d'Apprentissage</h4>
-               <Tag className='tag'>{model.hyperparameters.learningRate}</Tag>
-               
+              <h4 className="tag-title">Taux d'Apprentissage</h4>
+              <span className="badge bg-secondary">{model.hyperparameters.learningRate}</span>
             </div>
-            
             <div>
               <h4 className="tag-title">Nature de la Tâche</h4>
-              <Tag className='tag'>{model.taskNature}</Tag> 
+              <span className="badge bg-secondary">{model.taskNature}</span>
             </div>
-          
-            </Flex>
           </div>
+        </div>
       </div>
-      </div>
-      <div className="modal-footer"  style={{ display: 'flex', justifyContent: 'flex-end' }}>
-        <Button className="custom-button" onClick={onClose}>
+
+      <div className="modal-footer"  >
+        {/* <Button className="custom-button" onClick={onClose}>
           Fermer
-        </Button>
+        </Button> */}
+         <button type="button" className="custom-button" data-dismiss="modal" onClick={onClose}>Fermer</button>
       </div>
-    </Modal>
+    
+    
+    </div>
+  </div>
+</div>
   );
 }
 
