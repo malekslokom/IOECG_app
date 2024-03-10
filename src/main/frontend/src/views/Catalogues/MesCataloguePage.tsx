50778c3c73c8b4a09a830ca9e04910a52cadad22
malekslokom <slokom.malek@gmail.com>
2024-03-10 09:13:37 +0100

    model_filter_frontend

--- src/main/frontend/src/views/Catalogues/MesCataloguePage.tsx
+++ src/main/frontend/src/views/Catalogues/MesCataloguePage.tsx
@@ -3,6 +3,7 @@ import { fetchModels } from '../../services/ApiService';
 import ModelCard from '../../components/Card/ModelCard';
 import ModelModal from '../../components/Modals/ModelModal';
 import './CataloguePage.css'
+import HeaderModel from '../../components/HeaderList/HeaderModel';
 
 const MesCataloguePage: React.FC = () => {
   const [models, setModels] = useState<Model[]>([]);
@@ -46,6 +47,7 @@ const MesCataloguePage: React.FC = () => {
             <h2>Catalogue des modéles</h2>
         </div>
         <hr />
+        <HeaderModel></HeaderModel>
         <div className="model-list-container">
           <div className="row row-cols-1 row-cols-md-3">
           {models.map(model => (
